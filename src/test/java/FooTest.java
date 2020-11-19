61ec071f8c67fcf402cb09251caba524c7152eb7
Sonny George <355656+sonnyg@users.noreply.github.com>
2020-11-19 13:05:25 -0600

    instruction 2.1

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,39 +1,62 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class FooTest {
 
+    private ISuffixService suffixService;
+
+    @Before
+    public void setUp() {
+        suffixService = mock(ISuffixService.class);
+    }
+
     @Test
-    public void appendToReturnsValue() {
-        final Foo foo = new Foo("cod");
+    public void appendToUsesServiceToGetSuffix() {
+        final Foo foo = new Foo(suffixService);
+
+        when(suffixService.getSuffix(anyInt())).thenReturn("cod");
 
         assertThat(foo.appendTo("ing")).isEqualTo("coding");
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void appendToExpectsNonNullArgument() {
-        new Foo("foo").appendTo(null);
+    @Test
+    public void appendToUsesLengthOfRoot() {
+        final Foo foo = new Foo(suffixService);
+
+        when(suffixService.getSuffix(4)).thenReturn("co");
+
+        assertThat(foo.appendTo("ding")).isEqualTo("coding");
     }
 
-    @Test
-    public void fooInstancesCanHaveDifferentSuffixes() {
-        new Foo("foo");
-        new Foo("bar");
+    @Test(expected = IllegalArgumentException.class)
+    public void appendToExpectsNonNullArgument() {
+        new Foo(suffixService).appendTo(null);
     }
 
     static class Foo {
-        private final String _suffix;
+        private final ISuffixService suffixService;
 
-        public Foo(String _suffix) {
-            this._suffix = _suffix;
+        public Foo(ISuffixService suffixService) {
+            this.suffixService = suffixService;
         }
 
         public String appendTo(String root) {
-            return _suffix + Optional.ofNullable(root)
+            final String nonNullRoot = Optional.ofNullable(root)
                     .orElseThrow(() -> new IllegalArgumentException("root cannot be null"));
+            final String suffix = suffixService.getSuffix(nonNullRoot.length());
+
+            return suffix + nonNullRoot;
         }
+    }
+
+    interface ISuffixService {
+        String getSuffix(int length);
     }
 }
